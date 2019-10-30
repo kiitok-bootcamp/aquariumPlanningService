ad0e7f293d96ff9954136ee68d8fef2d066be5b7
miriwo0104 <shunokawa@gmail.com>
2019-10-30 22:51:53 +0900

    add: Made users controllers.
    Login page was made.
    New page was made.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "users/login" => "users#login"
+  get "users/new" => "users#new"
   get "/" => "home#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
