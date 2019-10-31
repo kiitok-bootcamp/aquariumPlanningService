b3f2fb6111acc8a25686f2a0d222940e6fabb212
miriwo0104 <shunokawa@gmail.com>
2019-10-31 21:35:53 +0900

    add: Registration users.
    Registration user function was added to this service.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,18 @@
 class UsersController < ApplicationController
   def login
+
   end
 
   def new
+
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def create
+    @user = User.new(name: params[:name], email: params[:email])
+    @user.save
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-    set_primary_key :id
     validates :name, {presence: true}
     validates :email, {presence: true}
-    validates :email, {uniquencess: true}
+    validates :email, {uniqueness: true}
 end
