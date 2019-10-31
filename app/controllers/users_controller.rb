dd32cf698be3180e2be0075b764f6ee9eb0eaf89
miriwo0104 <shunokawa@gmail.com>
2019-10-31 22:52:43 +0900

    change: Sign up page URL.
    Sign up page was changed URL and action name.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   end
 
-  def new
+  def signup
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   post "users/create" => "users#create"
   get "users/login" => "users#login"
-  get "users/new" => "users#new"
+  get "users/signup" => "users#signup"
   get "users/index" => "users#index"
   get "users/:id" => "users#show"
   get "/" => "home#top"
